0d0d6a4777faaeba27e8330dcf98b327139e92f0
Prince <mac@Prince.local>
2020-09-19 09:46:19 +0100

    login feature with Spring security

--- src/main/java/com/codeprince/employeemanagementsystem/controllers/EmployeeController.java
+++ src/main/java/com/codeprince/employeemanagementsystem/controllers/EmployeeController.java
@@ -5,10 +5,7 @@ import com.codeprince.employeemanagementsystem.services.EmployeeService;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,7 +23,7 @@ public class EmployeeController {
     public String viewHomePage(Model model) {
 //        Employee employee = new Employee();
 //        model.addAttribute("employee", employee);
-        return findPaginated(1, model);
+        return findPaginated(1, "firstName", "asc",  model);
     }
 
     @PostMapping("/saveEmployee")
@@ -43,6 +40,7 @@ public class EmployeeController {
 
         // Set the retrieved employee object to the model to prepopulate the form
         model.addAttribute("employee", employee);
+        System.out.println("EMPLOYEES DETAILS>>>>>>>>>" + employee);
 //        model.addAttribute("isAdd", false);
         return "update_employee";
     }
@@ -63,17 +61,26 @@ public class EmployeeController {
         return "redirect:/";
     }
 
+    // Contructing url similar to: /page/1?sortField=name&sortDir=asc
     @GetMapping("/page/{pageNo}")
-    public String findPaginated(@PathVariable (value = "pageNo") int pageNo, Model model) {
+    public String findPaginated(@PathVariable (value = "pageNo") int pageNo,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortField") String sortDir,
+                                Model model) {
         Employee employee = new Employee();
         model.addAttribute("employee", employee);
 
         int pageSize = 6;
-        Page<Employee> page = employeeService.findPaginated(pageNo, pageSize);
+        Page<Employee> page = employeeService.findPaginated(pageNo, pageSize, sortField, sortDir);
         List<Employee> listEmployees = page.getContent();
         model.addAttribute("currentPage", pageNo);
         model.addAttribute("totalPages", page.getTotalPages());
         model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
         model.addAttribute("listEmployees", listEmployees);
         return "index";
 
--- src/main/java/com/codeprince/employeemanagementsystem/models/Employee.java
+++ src/main/java/com/codeprince/employeemanagementsystem/models/Employee.java
@@ -3,39 +3,47 @@ package com.codeprince.employeemanagementsystem.models;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-
 @Data
-@Table
-@Entity(name = "employees")
+@Entity
+@Table(name = "employees")
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @NotNull
-    @Column(name = "first_name")
+    @NotEmpty
+    @NotBlank
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    @NotNull
     @NotEmpty
     @Column(name = "last_name")
     private String lastName;
 
     @NotEmpty
+    @Column(unique = true)
     private String email;
 
+    @NotNull
     @NotEmpty
     private String password;
 
+    @NotNull
     @NotEmpty
     private String gender;
 
+    @NotNull
     @NotEmpty
     private String address;
 
+    @NotNull
     @NotEmpty
     private String department;
 }
